dd549211018cc2f7ed623198ea36b66ded907dc0
Andhros <andhros@usp.br>
2020-12-03 21:44:09 -0300

    Testando artigo novo

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # Data/Path Handling
 import logging
+import pandas as pd
 import pickle
 import numpy as np
 from pydantic import BaseModel
@@ -8,6 +9,7 @@ import os
 # Server
 import uvicorn
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 
 # Modeling
 from xgboost import XGBRegressor
@@ -44,18 +46,35 @@ class Data(BaseModel):
     G1: int
     G2: int
 
+
 # está fudido precisa olhar aqui embaixo
-@app.post("/predict")
+# @app.post("/predict/")
+# async def predict(data: Data):
+#     # return data
+#     try:
+#         # Extract data in correct order
+#         to_predict = [data[feature] for feature in features]
+
+#         # Create and return prediction
+#         prediction = model.predict(np.array(to_predict).reshape(1, -1))
+#         return {"prediction": int(prediction[0])}
+
+#     except:
+#         my_logger.error("Something went wrong!")
+#         return {"prediction": "error"}
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+@app.post("/predict/")
 def predict(data: Data):
-    try:
-        # Extract data in correct order
-        data_dict = data.dict()
-        to_predict = [data_dict[feature] for feature in features]
+    # Extract data in correct order
+    data_dict = jsonable_encoder(data)
+    for key, value in data_dict.items():
+        data_dict[key] = [value]
+        to_predict = pd.DataFrame.from_dict(data_dict)
 
-        # Create and return prediction
-        prediction = model.predict(to_predict.reshape(1, -1))
-        return {"prediction": int(prediction[0])}
-
-    except:
-        my_logger.error("Something went wrong!")
-        return {"prediction": "error"}
+    # Create and return prediction
+    prediction = model.predict(to_predict)
+    return prediction[0]
