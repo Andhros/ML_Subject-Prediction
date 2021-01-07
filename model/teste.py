c02bdce7b693a4c0821ce38112f0c827228cbfe2
David Rocha <davidgr.bio@gmail.com>
2021-01-07 11:35:45 -0300

    comments and readme

--- model/teste.py
+++ model/teste.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+# dictionary for post the data and get the prediction
 to_predict = {
 "age": 21,
 "Medu": 1,
@@ -19,10 +20,17 @@ to_predict = {
 "G2": 8
 }
 
+# dumps the dict into a json to make the post
 to_predict = json.dumps(to_predict)
 
 print(type(to_predict))
 
+# post to get predictions
 url = 'http://127.0.0.1:8000/predict/'
 r = requests.post(url, data=to_predict)
 r.json()
+
+# get to retrive the model's metrics
+url_metrics = 'http://127.0.0.1:8000/metrics/'
+m = requests.get(url_metrics)
+m.json()
